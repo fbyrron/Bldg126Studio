b2214ad7357fee913e819f97fb6d706c3ed21e5f
fbyrron <fbyrron19@gmail.com>
2023-04-23 20:04:16 +0800

    first commit

---
+++ send.php
@@ -0,0 +1,81 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    // $mail->Username = ($_POST["Email"]);
+    $mail->Username = 'sesame13579@gmail.com';
+    $mail->Password = 'fheiucndptntxkhe';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('sesame13579@gmail.com', $_POST["Name"]);
+    // $mail->setFrom('sesame13579@gmail.com', 'Bldg126 Studio');
+
+    $mail->addAddress('sesame13579@gmail.com');
+    // $mail->addAddress($_POST["Email"]);
+
+    $mail->isHTML(true);
+
+    $mail->Subject = $_POST["Concern"];
+    $mail->Body =($_POST["Message"]. '<br><br> Email from <i>'.$_POST["Name"].'</i> ('.$_POST["Email"].')');
+
+    // $mail->send();
+
+    echo
+    "
+    <script>
+    alert('Email Sent Successfully');
+    document.location.href = index.html
+    </script>
+    ";
+
+    if($mail->Send()) {
+        $autoRespond = new PHPMailer();
+     
+        $autoRespond->IsSMTP();
+        $autoRespond->CharSet    = 'UTF-8';
+        $autoRespond->SMTPDebug  = 0;
+        $autoRespond->SMTPAuth   = TRUE;
+        $autoRespond->SMTPSecure = "tls";
+        $autoRespond->Port       = 587;
+        $autoRespond->Username   = "sesame13579@gmail.com";
+        $autoRespond->Password   = "fheiucndptntxkhe";
+        $autoRespond->Host       = 'smtp.gmail.com';
+     
+        $autoRespond->isHTML(true);
+        $autoRespond->setFrom('sesame13579@gmail.com', 'Bldg126 Studio');
+        $autoRespond->addAddress($_POST["Email"]);
+        $autoRespond->Subject = "Thank you for contacting us!"; 
+        $autoRespond_template =
+            'Dear <b>' .$_POST["Name"]. '</b>,<br><br>
+            
+            Thank you for contacting Bldg126 Studio. We appreciate you taking the time to reach out to us.<br><br>
+            
+            Regarding your message:<br>
+            <i>"' .$_POST['Message']. '"</i><br><br>
+            
+            We have received your message and we are currently reviewing it. Our team will be in touch with you as soon as possible.<br><br>
+            
+            In the meantime, please feel free to browse our website <a href="bldg126studio.rf.gd">Bldg Studio</a> for more information about our products and services.<br><br>
+            
+            Thank you again for reaching out to us. We look forward to assisting you.<br><br>
+            
+            Best regards,<br>
+            <b>Bldg126 Studio</b>';
+
+        $autoRespond->Body= $autoRespond_template;    
+     
+        $autoRespond->Send(); 
+     }
+}
